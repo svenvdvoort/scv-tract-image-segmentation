4cc5745c141e5f1ad911de97be218752ba351c39
svenvdvoort <svenvdvoort@protonmail.com>
2022-05-05 21:16:02 +0200

    Add README and push to Kaggle functionality

---
+++ README.md
@@ -0,0 +1,19 @@
+# UW-Madison GI Tract Image Segmentation
+
+This repository contains files and code related to our submission to the [Tract Image Segmentation competition on Kaggle](https://www.kaggle.com/competitions/uw-madison-gi-tract-image-segmentation).
+We picked this challenge as a project for the TU Delft course Seminar Computer Vision by Deep Learning.
+
+## Setting up
+
+- Create an account on Kaggle and enroll in the competition.
+- Install and authenticate with Kaggle API, please see the [Kaggle API Getting Started](https://www.kaggle.com/docs/api#getting-started-installation-&-authentication) documentation.
+
+## Running code on Kaggle
+
+- Run the included Python script: `python push_to_kaggle.py`
+- **Important**: Only code in the `scv_notebook.ipynb` notebook is automatically executed on Kaggle. If one of the cells errors, the cells below are not executed. Utility functions can (only) be defined in the `scv_utility.py` script and can be imported in the notebook.
+- Tests are not executed on Kaggle.
+
+# Running tests locally
+
+TODO
---
+++ push_to_kaggle.py
@@ -0,0 +1,37 @@
+""" Python script that automates running the notebook and included scripts on Kaggle """
+import re
+import subprocess
+import time
+
+try:
+    kaggle_config_output = subprocess.check_output("kaggle config view", shell=True).decode()
+    username = re.search("username: .*\n", kaggle_config_output).group(0).split()[1]
+except Exception as e:
+    print(f"Could not get username from kaggle command: {e}")
+    exit()
+
+# Subsitute INSERT_USERNAME with actual Kaggle username in metadata files
+with open("notebook_metadata/kernel-metadata.json", "r") as f:
+    notebook_metadata = f.read()
+with open("lib_metadata/kernel-metadata.json", "r") as f:
+    lib_metadata = f.read()
+with open("notebook_metadata/kernel-metadata.json", "w") as f:
+    f.write(notebook_metadata.replace("INSERT_USERNAME", username))
+with open("lib_metadata/kernel-metadata.json", "w") as f:
+    f.write(lib_metadata.replace("INSERT_USERNAME", username))
+
+subprocess.run("kaggle kernels push -p ./lib_metadata/", shell=True)
+print("SCV utility script was pushed. Now waiting 30 sec for it to become available...")
+for _ in range(30):
+    time.sleep(1)
+    print(".", end="", flush=True)
+print()
+subprocess.run("kaggle kernels push -p ./notebook_metadata", shell=True)
+
+# Revert to original metadata files
+with open("notebook_metadata/kernel-metadata.json", "w") as f:
+    f.write(notebook_metadata)
+with open("lib_metadata/kernel-metadata.json", "w") as f:
+    f.write(lib_metadata)
+
+print("Push to Kaggle complete")
